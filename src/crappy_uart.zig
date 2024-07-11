650b9b94c65dfd13ce6f59f9e1d3abfdaf9e0b02
Joseph S. Pacheco-Corwin <hammersamatom@gmail.com>
2024-07-11 07:26:23 -0400

    We needed some kind of input/output
    Might as well make it MMIO. Currently throws an Out Of Bounds error on
    the memory access. Confuses the shit out of me but whatever. I even
    hardcoded it to a value within the bounds and it failed. Zig magic?

---
+++ src/crappy_uart.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+
+pub const uart_settings = struct {
+    uart_status_address: u32,
+    counted: u8,
+    buffer_address: u32,
+};
+
+pub fn uart_transmit(properties: *uart_settings, memory: []u8) !void {
+    std.debug.print("uart_status_address = {x}\n", .{properties.*.uart_status_address});
+    std.debug.print("counted = {}\n", .{properties.*.counted});
+    std.debug.print("buffer_address = {x}\n", .{properties.*.buffer_address});
+
+    std.debug.print("Memory Size = {}\n", .{memory.len});
+
+    const total_bytes: *u8 = &memory[0x7ffff00];
+    const counted: *u8 = &properties.*.counted;
+
+    // If we've already iterated over everything, set the memory address pointed to by total_bytes to 0
+    std.debug.print("counted.* = {}\n", .{counted.*});
+    std.debug.print("total_bytes.* = {}\n", .{total_bytes.*});
+    if (counted.* == total_bytes.*) {
+        total_bytes.* = 0;
+        return;
+    }
+
+    try stdout.print("{}", .{properties.*.buffer_address + counted.*});
+    counted.* += 1;
+}
