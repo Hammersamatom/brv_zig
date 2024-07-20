f80b2bcdf772d2b42ab0865aa12c8fc0c21a96f2
Hammersamatom <hammersamatom@gmail.com>
2024-07-20 15:12:41 -0400

    Added boilerplate to main.zig to get real output
    Core halted conditions, UART printing after halt, etc

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 const rv = @import("cpu.zig");
+const types = @import("rv_types.zig");
 const uart = @import("crappy_uart.zig");
 
 pub fn main() !void {
@@ -18,7 +19,7 @@ pub fn main() !void {
     defer fileHandler.close();
 
     // Allocate the memory on the heap. Stack isn't large enough (1 << 23) - (1 << 14).
-    const mem_max: u32 = (1 << 32) - 1;
+    const mem_max: u32 = (1 << 23) - 0;
     const memory = try allocator.alloc(u8, mem_max);
     @memset(memory, 0);
     defer allocator.free(memory);
@@ -30,9 +31,20 @@ pub fn main() !void {
     var core_one: rv.cpu_state = rv.cpu_state{ .pc_reg = 0, .gp_regs = [_]u32{0} ** 32, .halted = false };
     var uart_prop: uart.uart_settings = uart.uart_settings{ .uart_status_address = 0x7f_ff00, .buffer_address = 0x7f_ff04, .counted = 0 };
 
-    while (core_one.halted == false) {
-        try rv.step_cpu(&core_one, memory);
+    while (true) {
+        if (core_one.halted == false) {
+            //std.debug.print("\npc_reg: {x:>10}", .{core_one.pc_reg});
+            //for (core_one.gp_regs, 0..) |reg, i| {
+            //    if (i % 2 == 0) std.debug.print("\n", .{});
+            //    std.debug.print("{s:<6}: {:<10} 0x{x:<10}", .{ types.abi[i], reg, reg });
+            //}
+            //std.debug.print("\n", .{});
+            try rv.step_cpu(&core_one, memory);
+        }
         try uart.uart_transmit(&uart_prop, memory);
+        //_ = try std.io.getStdIn().reader().readByte();
+        if (core_one.halted == true and memory[uart_prop.uart_status_address] == 0)
+            break;
     }
 
     const outFile = try std.fs.cwd().createFile("memory.bin", .{});
